09b69aa45da5ad4b8724c4b6eebe6b8e9964fc53
SunWuyuan <SunWuyuan@users.noreply.github.com>
2025-04-05 06:21:32 +0000

    deploy: 02c8fccb30845e4c4a17a40b9430c0bb4112b014

--- assets/ssrBoot-C9XVkltt.js
+++
@@ -1,1 +0,0 @@
-import{s,ad as t,f as e,bO as n}from"./index-CZWOVMJZ.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
