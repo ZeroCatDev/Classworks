9b120ec84988cd513cb6fc2ecaead117d271bb83
Sunwuyuan <Sunwuyuan@users.noreply.github.com>
2025-04-19 12:30:44 +0000

    deploy: eefd9eb0547c311c6beccf118b8fee0f0a977127

---
+++ assets/ssrBoot-pSHlH0et.js
@@ -0,0 +1,1 @@
+import{s,ac as t,f as e,bn as n}from"./index-8poTKozu.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
