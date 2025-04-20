aecf9e87cfd6ec733b33286cb843621c3173b6dd
Sunwuyuan <Sunwuyuan@users.noreply.github.com>
2025-04-20 02:45:59 +0000

    deploy: 59a9f1bd912b1d13e964f1c68fdb685ef8d81a69

---
+++ assets/ssrBoot-rqF4uojT.js
@@ -0,0 +1,1 @@
+import{s,ac as t,f as e,b1 as n}from"./index-Pb2Eku_S.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
